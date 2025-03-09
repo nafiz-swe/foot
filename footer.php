341e591eb5ce4b0b1f5af3967842a6eec78d7518
Nafizul Islam <96583087+nafiz-swe@users.noreply.github.com>
2025-03-09 14:05:29 +0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,545 @@
+<style>
+.footer-wave-path {
+  fill: #fff;
+}
+.footer-services{
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  border-top: 1px solid #edf0f5 !important;
+  padding: 15px 0 30px 0;
+  margin: auto auto 15px auto;
+}
+.footer-wave-svg {
+  background-color: transparent;
+  display: block;
+  height: 50px;
+  position: relative;
+  top: -1px;
+  width: 100%;
+}
+.footer-social-links {
+  bottom: -3px;
+  height: 54px;
+  position: absolute;
+  right: 3px;
+  width: 236px;
+  margin: auto 15px;
+}
+.footer-social-amoeba-svg {
+  height: 54px;
+  left: 0;
+  display: block;
+  position: absolute;
+  top: 0;
+  width: 236px;
+}
+.footer-social-amoeba-path {
+  fill: #243342;
+}
+.footer-social-link {
+  display: block;
+  padding: 10px;
+  position: absolute;
+}
+.footer-social-icon-svg {
+  display: block;
+}
+.footer-social-icon-path {
+  fill: #fffff2;
+  transition: fill .2s;
+}
+.footer-social-link.facebook {
+  height: 6px;
+  left: 0px;
+  top: 4px;
+  width: 26px;
+  font-size: 25px;
+  color: #fff;
+}
+.footer-social-link.whatsapp {
+  height: 28px;
+  left: 59px;
+  top: -5px;
+  width: 28px;
+  font-size: 27px;
+  color: #fff;
+}
+.footer-social-link.instagram {
+  height: 24px;
+  left: 119px;
+  top: 3px;
+  width: 24px;
+  font-size: 25px;
+  color: #fff;
+}
+.footer-social-link.youtube {
+  height: 34px;
+  left: 172px;
+  top: 2px;
+  width: 34px;
+  font-size: 25px;
+  color: #fff;
+}
+.ft-service{
+  margin: auto;
+}
+li {
+  list-style: none;
+}
+.border-top {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 40px 15px;
+  border-top: 1px solid #edf0f5 !important;
+}
+.border-top p{
+  padding: 20px 0;
+  color: #ccc;
+}
+.ft-icon {
+  display: inline-flex;
+  gap: 10px; 
+}
+.ft-icon i {
+  padding: 4px 3px;
+  color: #ccc;
+  cursor: pointer;
+  font-size: 25px;
+}
+.ft-icon i:hover {
+  color: #fff;
+}
+.ft-link{
+  padding: 5px 0;
+  color: #dddddd;
+  font-size: 15px;
+}
+.ft-link i {
+  margin-left: 10px;
+}
+.ft-link:hover{
+  text-decoration: underline;
+  color: #fff;
+}
+.footer-links i {
+  background: #113d8a;
+  color: #fff;
+  border-radius: 5px;
+}
+.footer-section {
+  padding:0;
+  background: #142646 !important;
+}
+.footer-section p {
+  font-size: 15px;
+  color: #dddddd;
+  margin-bottom: 5px;
+}
+.copy-right{
+  width: 100%;
+  margin: 0 auto;
+  padding: 15px 0;
+  background: #243342;
+}
+.copy-right p {
+  width: 90%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 30px;
+  font-size: 15px;
+  color: #fff;
+}
+.footer-section h3 {
+  font-size: 1.3rem;
+  letter-spacing: .1rem;
+  text-transform: uppercase;
+  color: #fff;
+  margin-bottom: .8rem;
+  margin-top: 35px;
+  font-weight: 900;
+  text-decoration: none;
+}
+.footer-section .footer-links li {
+  margin-bottom: 5px;
+  font-size: 15px;
+}
+@media (max-width: 600px) {
+.footer-section .footer-links li,
+.footer-section p {
+  font-size: 14px;
+}
+.border-top p {
+  margin-bottom: 30px;
+}
+}
+.foot-under{
+  background: #b7fc5f;
+  height: 3px;
+  width: 60px;
+}
+/*  body
+{
+  background: #000e29;
+} */
+.square_box {
+    position: absolute;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+    border-top-left-radius: 45px;
+    opacity: 0.302;
+}
+
+/* Box Three */
+.square_box.box_three {
+    background-image: -moz-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
+    background-image: -webkit-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
+    background-image: -ms-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
+    opacity: 0.059;
+    left: 70px;
+    top: 2px;
+    width: 450px;
+    height: 450px;
+    border-radius: 0px;
+    z-index: -1;
+}
+
+.square_box.box_four {
+    background-image: -moz-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
+    background-image: -webkit-linear-gradient(-270deg, #19bfff 0%, #f43d7e 100%);
+    background-image: -ms-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
+    opacity: 0.059;
+    left: 550px;
+    top: 61px;
+    width: 490px;
+    height: 490px;
+    border-radius: 0px;
+    z-index: -1;
+}
+
+.square_box.box_five {
+    background-image: -moz-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
+    background-image: -webkit-linear-gradient(-90deg, #19bfff 0%, #f43d7e 100%);
+    background-image: -ms-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
+    opacity: 0.059;
+    position: absolute;
+    left: 26px;
+    top: 1px;
+    width: 490px;
+    height: 490px;
+    border-radius: 0px;
+    z-index: -1;
+}
+
+.square_box.box_six {
+    background-image: -moz-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
+    background-image: -webkit-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
+    background-image: -ms-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
+    opacity: 0.059;
+    left: 460px;
+    top: 42px;
+    width: 510px;
+    height: 510px;
+    border-radius: 0px;
+    z-index: -1;
+}
+
+/* Media Query for 1224px and below */
+@media (max-width: 1224px) {
+    .square_box.box_three {
+    left: 30px;
+    width: 250px;
+    height: 250px;
+    }
+    .square_box.box_four{
+    left: 190px;
+    width: 250px;
+    height: 250px;
+    }
+    .square_box.box_five {
+    left: 50px;
+    width: 250px;
+    height: 250px;
+    }
+    .square_box.box_six {
+    left: 170px;
+    width: 250px;
+    height: 250px;
+    }
+}
+/* Media Query for 500px and below */
+@media (max-width: 500px) {
+    .square_box.box_three {
+    left: 30px;
+    width: 200px;
+    height: 200px;
+    }
+    .square_box.box_four{
+    left: 190px;
+    width: 200px;
+    height: 200px;
+    }
+    .square_box.box_five {
+    left: 50px;
+    width: 200px;
+    height: 200px;
+    }
+    .square_box.box_six {
+    left: 170px;
+    width: 200px;
+    height: 200px;
+    }
+}
+/* Media Query for 440px and below */
+@media (max-width: 440px) {
+    .square_box.box_three {
+    left: 10px;
+    width: 170px;
+    height: 170px;
+    }
+    .square_box.box_four{
+    left: 120px;
+    width: 170px;
+    height: 170px;
+    }
+    .square_box.box_five {
+    left: 20px;
+    width: 170px;
+    height: 170px;
+    }
+    .square_box.box_six {
+    left: 120px;
+    width: 170px;
+    height: 170px;
+    }
+}
+  /* Media Query for 350px and below */
+@media (max-width: 350px) {
+    .square_box.box_three {
+    left: 8px;
+    width: 150px;
+    height: 150px;
+    }
+    .square_box.box_four{
+    left: 90px;
+    width: 150px;
+    height: 150px;
+    }
+    .square_box.box_five {
+    left: 15px;
+    width: 150px;
+    height: 150px;
+    }
+    .square_box.box_six {
+    left: 90px;
+    width: 150px;
+    height: 150px;
+    }
+}
+  #scroll-up-btn {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    cursor: pointer;
+    padding: 7px 14px;
+    background: linear-gradient(#113d8a, #083882);
+    color: #fff;
+    border-radius: 2px;
+    transition: transform .5s;
+    z-index: 1;
+}
+</style>
+<footer class="footer-section bg-white">
+      <svg class="footer-wave-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 100" preserveAspectRatio="none">
+        <path class="footer-wave-path" d="M851.8,100c125,0,288.3-45,348.2-64V0H0v44c3.7-1,7.3-1.9,11-2.9C80.7,22,151.7,10.8,223.5,6.3C276.7,2.9,330,4,383,9.8 c52.2,5.7,103.3,16.2,153.4,32.8C623.9,71.3,726.8,100,851.8,100z"></path>
+      </svg>
+      <div class="container" data-aos="fade-down">
+                    <div class="square_box box_three"></div>
+          			<div class="square_box box_four"></div>
+        <div class="row" style="margin: 50px auto 30px auto;">
+          <div class="col-md-4" style="text-align: left;">
+            <h3>About EuroZoom</h3>
+            <div class="foot-under" style="margin: -12px auto 10px 0;"></div>
+            <p>We provide expert assistance for European language learning, study, and work opportunities. From managing offer letters to guiding your journey, we strive to make your dreams of Europe a reality.</p>
+            <div class="site-logo mr-auto w-20"><a href="index.php"><img src="webimg/EuroZoom-footer.svg" style="width:134px; height:50px; border-radius:5px; margin:20px auto 20px 0;"></a></div>
+          </div>
+          <div class="col-md-4" style="text-align: center;">
+            <h3>Our Services</h3>
+            <div class="foot-under" style="margin: -12px auto 10px auto;"></div>
+            <ul class="list-unstyled footer-links">
+              <li><a href="reviews.php" class="ft-link">Services 1</a></li>
+              <li><a href="reviews.php" class="ft-link">Services 2</a></li>
+              <li><a href="reviews.php" class="ft-link">Services 3</a></li>
+              <li><a href="reviews.php" class="ft-link">Services 4</a></li>
+              <li><a href="reviews.php" class="ft-link">Services 5</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4" style="text-align: right;">
+            <h3>Quick Links</h3>
+            <div class="foot-under" style="margin: -12px 0 10px auto;"></div>
+            <ul class="list-unstyled footer-links">
+              <li><a href="#home-section" class="ft-link">Term & Conditions</a></li>
+              <li><a href="#home-section" class="ft-link">Privacy Policy</a></li>
+              <li><a href="#home-section" class="ft-link">About Us</a></li>
+              <li><a href="reviews.php" class="ft-link">Reviews</a></li>
+              <li><a href="#home-section" class="ft-link">Blogs</a></li>
+            </ul>
+          </div>
+        </div>
+        
+        <div class="row" style="margin: 30px auto 30px auto; border-top: 1px solid #edf0f5; padding-top: 15px;">
+          <div class="col-md-4" style="text-align: left;">
+                     <div class="square_box box_five"></div>
+                     <div class="square_box box_six"></div>
+            <h3>Our Services</h3>
+            <div class="foot-under" style="margin: -12px auto 10px 0;"></div>
+            <ul class="list-unstyled footer-links">
+              <li><a href="country/germany.php" class="ft-link">Germany Services</a></li>
+              <li><a href="country/finland.php" class="ft-link">Finland Services</a></li>
+              <li><a href="country/france.php" class="ft-link">France Services</a></li>
+              <li><a href="country/russia.php" class="ft-link">Russia Services</a></li> 
+              <li><a href="country/spain.php" class="ft-link">Spain Services</a></li>
+              <li><a href="country/italy.php" class="ft-link">Italy Services</a></li>
+              <li><a href="country/uk.php" class="ft-link">UK Services</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 ml-auto" style="text-align: center;">
+            <h3>Our Services</h3>
+            <div class="foot-under" style="margin: -12px auto 10px auto;"></div>
+            <ul class="list-unstyled footer-links">
+              <li><a href="country/switzerland.php" class="ft-link">Switzerland Services</a></li>
+              <li><a href="country/austria.php" class="ft-link">Austria Services</a></li>
+              <li><a href="country/hungary.php" class="ft-link">Hungary Services</a></li>
+              <li><a href="country/belgium.php" class="ft-link">Belgium Services</a></li>
+              <li><a href="country/denmark.php" class="ft-link">Denmark Services</a></li>
+              <li><a href="country/portugal.php" class="ft-link">Portugal Services</a></li>
+              <li><a href="country/sweden.php" class="ft-link">Sweden Services</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4" style="text-align: right;">
+            <h3>Our Services</h3>
+            <div class="foot-under" style="margin: -12px 0 10px auto;"></div>
+            <ul class="list-unstyled footer-links">
+              <li><a href="country/netherlands.php" class="ft-link">Netherlands Services</a></li>
+              <li><a href="country/ireland.php" class="ft-link">Ireland Services</a></li>
+              <li><a href="country/poland.php" class="ft-link">Poland Services</a></li>
+              <li><a href="country/lithuania.php" class="ft-link">Lithuania Services</a></li>
+              <li><a href="country/romania.php" class="ft-link">Romania Services</a></li>
+              <li><a href="country/croatia.php" class="ft-link">Croatia Services</a></li>
+              <li><a href="country/greece.php" class="ft-link">Greece Services</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="footer-services">
+          <div class="col-md-4 ft-service" style="text-align: left;">
+            <h3>Direct Contact</h3>
+            <div class="foot-under" style="margin: -12px auto 10px 0;"></div>
+            <p> eurozoom.net@gmail.com</p>
+            <p> 01737226404 (Only WhatsApp)</p>
+          </div>
+          <div class="col-md-4 ft-service" style="text-align: center;">
+            <h3>Office Time</h3>
+            <div class="foot-under" style="margin: -12px auto 10px auto;"></div>
+            <p> Germany Time: Sat-Thu (8am-8pm)</p>
+            <p> Bangladesh Time: Sat-Thu (11am-11pm)</p>
+          </div>
+          <div class="col-md-4 ft-service" style="text-align: right;">
+            <h3>Questions & Answers</h3>
+            <div class="foot-under" style="margin: -12px 0 10px auto;"></div>
+            <li><a href="https://www.youtube.com/@EuroZoom" target="_blank" class="ft-link">Youtube Blog (Every Monday)</a></li>
+            <li><a href="https://www.zoom.com/EuroZoom" target="_blank" class="ft-link">Zoom Meeting (Every Friday 9pm BD)</a></li>
+          </div>
+        </div>
+
+        <div class="border-top">
+          <div class="footer-social-links">
+            <svg class="footer-social-amoeba-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 236 54">
+              <path class="footer-social-amoeba-path" d="M223.06,43.32c-.77-7.2,1.87-28.47-20-32.53C187.78,8,180.41,18,178.32,20.7s-5.63,10.1-4.07,16.7-.13,15.23-4.06,15.91-8.75-2.9-6.89-7S167.41,36,167.15,33a18.93,18.93,0,0,0-2.64-8.53c-3.44-5.5-8-11.19-19.12-11.19a21.64,21.64,0,0,0-18.31,9.18c-2.08,2.7-5.66,9.6-4.07,16.69s.64,14.32-6.11,13.9S108.35,46.5,112,36.54s-1.89-21.24-4-23.94S96.34,0,85.23,0,57.46,8.84,56.49,24.56s6.92,20.79,7,24.59c.07,2.75-6.43,4.16-12.92,2.38s-4-10.75-3.46-12.38c1.85-6.6-2-14-4.08-16.69a21.62,21.62,0,0,0-18.3-9.18C13.62,13.28,9.06,19,5.62,24.47A18.81,18.81,0,0,0,3,33a21.85,21.85,0,0,0,1.58,9.08,16.58,16.58,0,0,1,1.06,5A6.75,6.75,0,0,1,0,54H236C235.47,54,223.83,50.52,223.06,43.32Z"></path>
+            </svg>
+
+            <!-- Facebook -->
+            <a id="facebookLink" class="footer-social-link facebook" href="https://www.facebook.com/ShosurBari.Matrimony" target="_blank" aria-label="Visit ShosurBari on Facebook">
+              <img src="webimg/Facebook-EuroZoom.png" alt="Facebook Logo" style="width: 32px; height: 32px; border-radius: 50%;">
+            </a>
+
+            <!-- WhatsApp -->
+            <a class="footer-social-link whatsapp" href="https://wa.me/8801737226404" target="_blank" aria-label="Nafiz WhatsApp">
+              <img src="webimg/Whatsapp-EuroZoom.png" alt="WhatsApp Logo" style="width: 32px; height: 32px; border-radius: 50%;">
+            </a>
+
+            <!-- Gmail / Email -->
+            <a class="footer-social-link instagram" href="mailto:info@shosurbari.com" target="_blank" aria-label="ShosurBari Email">
+              <img src="webimg/Email-EuroZoom.png" alt="Gmail Logo" style="width: 32px; height: 32px; border-radius: 50%;">
+            </a>
+
+            <!-- YouTube -->
+            <a id="youtubeLink" class="footer-social-link youtube" href="https://www.youtube.com/@EuroZoom-Nafiz/videos" target="_blank" aria-label="YouTube">
+              <img src="webimg/Youtube-EuroZoom.png" alt="YouTube Logo" style="width: 32px; height: 32px; border-radius: 50%;">
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="copy-right">
+        <p> Copyright © 2025-26 EuroZoom.net | All Rights Reserved | Developed by <a href="https://api.whatsapp.com/send/?phone=8801737226404&text&type=phone_number&app_absent=0" target="_blank" style="color: #b7fc5f; font-weight: bold;" aria-label="Nafiz WhatsApp">Nafiz</a></p>
+        <!-- <p> Copyright &copy; 2025 All rights reserved EuroZoom</p> <script>document.write(new Date().getFullYear());</script>-->
+      </div>
+    </footer>
+
+
+
+
+	<!--Scrolling Bottom to top -->
+	<div id="scroll-up-btn" onclick="scrollToTop()">
+		<i class="fa fa-angle-double-up"></i>
+	</div>
+<!--All Page Scrolling Bottom to top -->
+<script>
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+	var scrollBtn = document.getElementById("scroll-up-btn");
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	scrollBtn.style.display = "block";
+	} else {
+	scrollBtn.style.display = "none";
+	}
+}
+function scrollToTop() {
+	var scrollOptions = {
+	top: 0,
+	left: 0,
+	behavior: 'smooth'
+};
+document.documentElement.scrollTo(scrollOptions);
+}
+</script>
+<!-- Navigation Bar Bottom Shadow when scroll down -->
+<script>
+	window.addEventListener("scroll", function() {
+		var navbar = document.querySelector(".navbar-inner_1");
+		if (window.pageYOffset > 50) {
+		navbar.style.boxShadow = "0 -3px 12px 0 #000";
+		} else {
+		navbar.style.boxShadow = "none";
+		}
+	});
+</script>
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<script>
+  // Check if the user is on a mobile device
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  // Get the social link elements
+  const facebookLink = document.getElementById('facebookLink');
+  const youtubeLink = document.getElementById('youtubeLink');
+  // Define the mobile and desktop URLs for Facebook and YouTube
+  const facebookMobileUrl = 'fb://page/102433621463638';
+  const facebookDesktopUrl = 'https://www.facebook.com/ShosurBari.Matrimony';
+  const youtubeMobileUrl = 'vnd.youtube://www.youtube.com/channel/UCW9JdUZQfadIZbnFBACc7hg'; //your_youtube_channel_id Replace with your YouTube channel ID
+  const youtubeDesktopUrl = 'https://www.youtube.com/@EuroZoom-Nafiz/videos';
+  // Set the href attribute based on the device type for Facebook
+  facebookLink.href = isMobile ? facebookMobileUrl : facebookDesktopUrl;
+  // Set the href attribute based on the device type for YouTube
+  youtubeLink.href = isMobile ? youtubeMobileUrl : youtubeDesktopUrl;
+</script>
